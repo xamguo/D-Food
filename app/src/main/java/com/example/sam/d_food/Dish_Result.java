bbc635020da78122ad35a7eac3f5dfa012e01ac1
xamguo <guoxiao113113@gmail.com>
2015-04-04 16:35:31 -0400

    Initial commit

---
+++ app/src/main/java/com/example/sam/d_food/Dish_Result.java
@@ -0,0 +1,138 @@
+package com.example.sam.d_food;
+
+import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class Dish_Result extends ListActivity {
+
+    private List<Map<String, Object>> mData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dish__result);
+
+        mData = getData();
+        MyAdapter adapter = new MyAdapter(this);
+        setListAdapter(adapter);
+    }
+
+    private List<Map<String, Object>> getData() {
+        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("title", "Mixed Greens Salad");
+        map.put("info", "$5.00");
+        list.add(map);
+
+        map = new HashMap<String, Object>();
+        map.put("title", "Grilled Escarole Salad");
+        map.put("info", "$7.50");
+        list.add(map);
+
+        map = new HashMap<String, Object>();
+        map.put("title", "Smoked Bluefish Pt");
+        map.put("info", "$7.00");
+        list.add(map);
+
+        map = new HashMap<String, Object>();
+        map.put("title", "Daily Double");
+        map.put("info", "$12.00");
+        list.add(map);
+
+        map = new HashMap<String, Object>();
+        map.put("title", "Pierogies");
+        map.put("info", "$14.00");
+        list.add(map);
+
+        map = new HashMap<String, Object>();
+        map.put("title", "Hamburger");
+        map.put("info", "$12.00");
+        list.add(map);
+        return list;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_dish__result, menu);
+        return true;
+    }
+
+    public class MyAdapter extends BaseAdapter {
+
+        private LayoutInflater mInflater;
+
+
+        public MyAdapter(Context context){
+            this.mInflater = LayoutInflater.from(context);
+        }
+        @Override
+        public int getCount() {
+            // TODO Auto-generated method stub
+            return mData.size();
+        }
+
+        @Override
+        public Object getItem(int arg0) {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        @Override
+        public long getItemId(int arg0) {
+            // TODO Auto-generated method stub
+            return 0;
+        }
+
+        public final class ViewHolder{
+            public ImageView img;
+            public TextView title;
+            public TextView info;
+            public Button viewBtn;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            ViewHolder holder = null;
+            if (convertView == null) {
+
+                holder=new ViewHolder();
+
+                convertView = mInflater.inflate(R.layout.dish_list, null);
+                holder.title = (TextView)convertView.findViewById(R.id.title);
+                holder.info = (TextView)convertView.findViewById(R.id.info);
+                convertView.setTag(holder);
+
+            }else {
+
+                holder = (ViewHolder)convertView.getTag();
+            }
+
+
+            holder.title.setText((String)mData.get(position).get("title"));
+            holder.info.setText((String)mData.get(position).get("info"));
+
+            return convertView;
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/sam/d_food/Resaurant_Result.java
@@ -0,0 +1,209 @@
+package com.example.sam.d_food;
+
+import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.*;
+import android.widget.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class Resaurant_Result extends ListActivity {
+
+    private List<Map<String, Object>> mData;
+    View justview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resaurant__result);
+
+        mData = getData();
+        MyAdapter adapter = new MyAdapter(this);
+        setListAdapter(adapter);
+
+    }
+
+    private List<Map<String, Object>> getData() {
+        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("title", "Phipps Conservatory and Botanical ");
+        map.put("info", "Chinese Restaurant $$");
+        map.put("img", R.mipmap.ic_launcher);
+        list.add(map);
+
+        map = new HashMap<String, Object>();
+        map.put("title", "Gaucho Parrilla Argentina");
+        map.put("info", "Bakeries, French $");
+        map.put("img",  R.mipmap.ic_launcher);
+        list.add(map);
+
+        map = new HashMap<String, Object>();
+        map.put("title", "Espresso A Mano");
+        map.put("info", "Coffee & Tea, Juice Bars & Smoothies $");
+        map.put("img",  R.mipmap.ic_launcher);
+        list.add(map);
+
+        map = new HashMap<String, Object>();
+        map.put("title", "La Gourmandine Bakery & Pastry Shop");
+        map.put("info", "Italian $$$$");
+        map.put("img",  R.mipmap.ic_launcher);
+        list.add(map);
+
+        map = new HashMap<String, Object>();
+        map.put("title", "Simpatico Espresso");
+        map.put("info", "French $$$");
+        map.put("img",  R.mipmap.ic_launcher);
+        list.add(map);
+
+        map.put("title", "Phipps Conservatory and Botanical ");
+        map.put("info", "Chinese Restaurant $$");
+        map.put("img", R.mipmap.ic_launcher);
+        list.add(map);
+
+        map = new HashMap<String, Object>();
+        map.put("title", "Gaucho Parrilla Argentina");
+        map.put("info", "Bakeries, French $");
+        map.put("img",  R.mipmap.ic_launcher);
+        list.add(map);
+
+        map = new HashMap<String, Object>();
+        map.put("title", "Espresso A Mano");
+        map.put("info", "Coffee & Tea, Juice Bars & Smoothies $");
+        map.put("img",  R.mipmap.ic_launcher);
+        list.add(map);
+
+        map = new HashMap<String, Object>();
+        map.put("title", "La Gourmandine Bakery & Pastry Shop");
+        map.put("info", "Italian $$$$");
+        map.put("img",  R.mipmap.ic_launcher);
+        list.add(map);
+
+        map = new HashMap<String, Object>();
+        map.put("title", "Simpatico Espresso");
+        map.put("info", "French $$$");
+        map.put("img",  R.mipmap.ic_launcher);
+        list.add(map);
+
+        map.put("title", "Phipps Conservatory and Botanical ");
+        map.put("info", "Chinese Restaurant $$");
+        map.put("img", R.mipmap.ic_launcher);
+        list.add(map);
+
+        map = new HashMap<String, Object>();
+        map.put("title", "Gaucho Parrilla Argentina");
+        map.put("info", "Bakeries, French $");
+        map.put("img",  R.mipmap.ic_launcher);
+        list.add(map);
+
+        map = new HashMap<String, Object>();
+        map.put("title", "Espresso A Mano");
+        map.put("info", "Coffee & Tea, Juice Bars & Smoothies $");
+        map.put("img",  R.mipmap.ic_launcher);
+        list.add(map);
+
+        map = new HashMap<String, Object>();
+        map.put("title", "La Gourmandine Bakery & Pastry Shop");
+        map.put("info", "Italian $$$$");
+        map.put("img",  R.mipmap.ic_launcher);
+        list.add(map);
+
+        map = new HashMap<String, Object>();
+        map.put("title", "Simpatico Espresso");
+        map.put("info", "French $$$");
+        map.put("img",  R.mipmap.ic_launcher);
+        list.add(map);
+        return list;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_resaurant__result, menu);
+        return true;
+    }
+
+    @Override
+    protected void onResume() {
+       if(justview != null)
+            justview.setBackgroundColor(Color.GRAY);
+        super.onResume();
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        justview = v;
+        v.setBackgroundColor(Color.BLUE);
+        Intent intent = new Intent(this,Dish_Result.class);
+        startActivity(intent);
+    }
+
+    public class MyAdapter extends BaseAdapter {
+
+        private LayoutInflater mInflater;
+
+
+        public MyAdapter(Context context){
+            this.mInflater = LayoutInflater.from(context);
+        }
+        @Override
+        public int getCount() {
+            // TODO Auto-generated method stub
+            return mData.size();
+        }
+
+        @Override
+        public Object getItem(int arg0) {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        @Override
+        public long getItemId(int arg0) {
+            // TODO Auto-generated method stub
+            return 0;
+        }
+
+        public final class ViewHolder{
+            public ImageView img;
+            public TextView title;
+            public TextView info;
+            public Button viewBtn;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            ViewHolder holder = null;
+            if (convertView == null) {
+
+                holder=new ViewHolder();
+
+                convertView = mInflater.inflate(R.layout.restaurant_list, null);
+                holder.img = (ImageView)convertView.findViewById(R.id.img);
+                holder.title = (TextView)convertView.findViewById(R.id.title);
+                holder.info = (TextView)convertView.findViewById(R.id.info);
+                convertView.setTag(holder);
+
+            }else {
+
+                holder = (ViewHolder)convertView.getTag();
+            }
+
+
+            holder.img.setBackgroundResource((Integer)mData.get(position).get("img"));
+            holder.title.setText((String)mData.get(position).get("title"));
+            holder.info.setText((String)mData.get(position).get("info"));
+
+            return convertView;
+        }
+    }
+}
