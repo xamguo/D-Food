937da8751ca4122a6fce50e111869b2ab4cd9ba4
xamguo <guoxiao113113@gmail.com>
2015-04-11 10:35:32 -0400

    Logo fixed

--- app/src/main/java/com/example/sam/d_food/presentation/main_page/HomePageActivity.java
+++ app/src/main/java/com/example/sam/d_food/presentation/main_page/HomePageActivity.java
@@ -1,23 +1,35 @@
 package com.example.sam.d_food.presentation.main_page;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.SeekBar;
 
 import com.example.sam.d_food.R;
+import com.example.sam.d_food.integration.service.DataService;
 import com.example.sam.d_food.presentation.login_page.LoginPageActivity;
 import com.example.sam.d_food.presentation.restaurant_page.RestaurantResultListActivity;
 
 
 public class HomePageActivity extends Activity {
+    DataService service;
+    boolean mBound = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState); //submission Test
         setContentView(R.layout.activity_home_page);
+
+        startService();
+
 
         SeekBar seekBar = (SeekBar)findViewById(R.id.homeSeekBar);
         seekBar.incrementProgressBy(10);
@@ -39,7 +51,7 @@ public class HomePageActivity extends Activity {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-
+                bindService();
             }
         });
     }
@@ -64,4 +76,36 @@ public class HomePageActivity extends Activity {
         Intent intent = new Intent(this,RestaurantResultListActivity.class);
         startActivity(intent);
     }
+
+    private void startService() {
+        startService(new Intent(this, DataService.class));
+    }
+
+    private void unbindService() {
+        if (mBound) {
+            unbindService(mConnection);
+            mBound = false;
+        }
+    }
+
+    private void bindService() {
+        Intent intent = new Intent(this, DataService.class);
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className,
+                                       IBinder service) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            mBound = true;
+            Log.v("connected service","xiao");
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            mBound = false;
+        }
+    };
 }
