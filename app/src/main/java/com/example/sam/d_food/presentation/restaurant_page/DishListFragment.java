13e631ee21ad7491074fafc9e2fb94ffa26f09e5
xamguo <guoxiao113113@gmail.com>
2015-04-25 22:31:13 -0400

    Connected to server2.0

--- app/src/main/java/com/example/sam/d_food/presentation/restaurant_page/DishListFragment.java
+++ app/src/main/java/com/example/sam/d_food/presentation/restaurant_page/DishListFragment.java
@@ -10,18 +10,24 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.example.sam.d_food.R;
+import com.example.sam.d_food.entities.data.RestaurantProxy;
 
 /**
  * Created by Sam on 4/16/2015.
  */
 public class DishListFragment extends ListFragment
 {
+    private int restaurantIndex;
 
     String[] numbers_text = new String[] { "one", "two", "three", "four",
             "five", "six", "seven", "eight", "nine", "ten", "eleven",
             "twelve", "thirteen", "fourteen", "fifteen" };
     String[] numbers_digits = new String[] { "1", "2", "3", "4", "5", "6", "7",
             "8", "9", "10", "11", "12", "13", "14", "15" };
+
+    public void setRestaurantIndex(int restaurantIndex) {
+        this.restaurantIndex = restaurantIndex;
+    }
 
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
@@ -38,10 +44,13 @@ public class DishListFragment extends ListFragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        /*
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(
                 inflater.getContext(), android.R.layout.simple_list_item_1,
                 numbers_text);
         setListAdapter(adapter);
+        */
+        setListAdapter(new CustomDishArrayAdapter(getActivity(), RestaurantProxy.getDishList(restaurantIndex)));
         return super.onCreateView(inflater, container, savedInstanceState);
     }
 }
