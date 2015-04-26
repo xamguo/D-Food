52001bb61b25ca800dfaeef2739e66a4af52b69c
Raymind <excelwayli@gmail.com>
2015-04-25 23:58:52 -0400

    Order1.1

--- app/src/main/java/com/example/sam/d_food/presentation/check_page/CheckActivity.java
+++ app/src/main/java/com/example/sam/d_food/presentation/check_page/CheckActivity.java
@@ -1,6 +1,7 @@
 package com.example.sam.d_food.presentation.check_page;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -11,13 +12,21 @@ import com.example.sam.d_food.R;
 import com.example.sam.d_food.presentation.intents.IntentToUserMap;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class CheckActivity extends Activity {
@@ -51,50 +60,105 @@ public class CheckActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
     public void usermap(View view) {
-        postOrder();
+        PlaceOrder order = new PlaceOrder();
+        order.execute("http://guoxiao113.oicp.net/D_Food_Server/place_order?");
         finish();
         IntentToUserMap intent = new IntentToUserMap(this);
         startActivity(intent);
     }
 
+    private class PlaceOrder extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost("http://guoxiao113.oicp.net/D_Food_Server/place_order?");
+            try {
+                // Add your data
+                Log.v("order placing", "...");
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(3);
+                nameValuePairs.add(new BasicNameValuePair("user_id", "1"));
+                nameValuePairs.add(new BasicNameValuePair("dish_id", "1"));
+                nameValuePairs.add(new BasicNameValuePair("dish_quantity", "1"));
+                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+                // Execute HTTP Post Request
+                HttpResponse response = httpclient.execute(httppost);
+                Log.v("place order", "success");
+            } catch (ClientProtocolException e) {
+                // TODO Auto-generated catch block
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+            }
+            return params[0];
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+        }
+    }
+
     private void postOrder() {
         InputStream inputStream = null;
         String result = "";
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost("http://guoxiao113.oicp.net/D_Food_Server/place_order?");
         try {
+            // Add your data
+            Log.v("order placing", "...");
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(3);
+            nameValuePairs.add(new BasicNameValuePair("user_id", "1"));
+            nameValuePairs.add(new BasicNameValuePair("dish_id", "1"));
+            nameValuePairs.add(new BasicNameValuePair("dish_quantity", "1"));
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
 
-            String url = "http://guoxiao113.oicp.net/D_Food_Server/place_order?";
-
-            // 1. create HttpClient
-            HttpClient httpclient = new DefaultHttpClient();
-
-            // 2. make POST request to the given URL
-            HttpPost httpPost = new HttpPost(url);
-
-            String json = "";
-
-            // 3. build jsonObject
-            JSONObject jsonObject = new JSONObject();
-            jsonObject.accumulate("user_id", "1");
-            jsonObject.accumulate("dish_id", "1");
-            jsonObject.accumulate("dish_quantity", "1");
-
-            // 4. convert JSONObject to JSON to String
-            json = jsonObject.toString();
-
-            // 5. set json to StringEntity
-            StringEntity se = new StringEntity(json);
-
-            // 6. set httpPost Entity
-            httpPost.setEntity(se);
-
-            // 7. Set some headers to inform server about the type of the content
-            httpPost.setHeader("Accept", "application/json");
-            httpPost.setHeader("Content-type", "application/json");
-
-            // 8. Execute POST request to the given URL
-            HttpResponse httpResponse = httpclient.execute(httpPost);
-        } catch (Exception e) {
-            Log.d("InputStream", "faled to place order");
+            // Execute HTTP Post Request
+            HttpResponse response = httpclient.execute(httppost);
+            Log.v("place order", "success");
+        } catch (ClientProtocolException e) {
+            // TODO Auto-generated catch block
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
         }
+//        try {
+//
+//            String url = "http://guoxiao113.oicp.net/D_Food_Server/place_order?";
+//
+//            // 1. create HttpClient
+//            HttpClient httpclient = new DefaultHttpClient();
+//
+//            // 2. make POST request to the given URL
+//            HttpPost httpPost = new HttpPost(url);
+//
+//            String json = "";
+//
+//            // 3. build jsonObject
+//            JSONObject jsonObject = new JSONObject();
+//            jsonObject.accumulate("user_id", "1");
+//            jsonObject.accumulate("dish_id", "1");
+//            jsonObject.accumulate("dish_quantity", "1");
+//
+//            // 4. convert JSONObject to JSON to String
+//            json = jsonObject.toString();
+//
+//            // 5. set json to StringEntity
+//            StringEntity se = new StringEntity(json);
+//
+//            // 6. set httpPost Entity
+//            httpPost.setEntity(se);
+//
+//            // 7. Set some headers to inform server about the type of the content
+//            httpPost.setHeader("Accept", "application/json");
+//            httpPost.setHeader("Content-type", "application/json");
+//
+//            // 8. Execute POST request to the given URL
+//            HttpResponse httpResponse = httpclient.execute(httpPost);
+//
+//            String resJson = EntityUtils.toString(httpResponse.getEntity(), "UTF-8");
+//            Log.v("result", resJson);
+//        } catch (Exception e) {
+//            Log.d("InputStream", "faled to place order");
+//        }
     }
 }
